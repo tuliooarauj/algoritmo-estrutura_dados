6d713bbdc18e00bb8b19a8ab13b09626368ff862
tuliooarauj <127243520+tuliooarauj@users.noreply.github.com>
2023-11-20 17:07:29 -0300

    create insert method

--- Arvores Binaris/binary_search_tree.py
+++ Arvores Binaris/binary_search_tree.py
@@ -25,37 +25,46 @@ class BinaryTree:
         if node.right:
             self.simetric_traversal(node.right)
             print(')', end='')
+
+    def postorder_traversal(self, node = None):
+        if node is None:
+            node = self.root #Partindo por padrão a partir da raiz
+        if node.left:
+            self.postorder_traversal(node.left)
+        if node.right:
+            self.postorder_traversal(node.right)
+        print(node, end='')
+
+    def height(self, node = None):
+        if node is None:
+            node = self.root #Partindo por padrão a partir da raiz
+        hleft = 0
+        hright = 0
+        if node.left:
+            hleft = self.height(node.left)
+        if node.right:
+            hright = self.height(node.right)
+        if hright > hleft:
+            return hright + 1 
+        return hleft + 1
+    
+class BinarySearchTree(BinaryTree):
+    
+    def insert(self, value):
+        parent = None
+        x = self.root
+        while x:
+            parent = x
+            if value < x.data:
+                x = x.left
+            else:
+                x = x.right
+        if parent is None:
+            self.root = TreeNode(value)
+        elif value < parent.data:
+            parent.left = TreeNode(value)
+        else:
+            parent.right = TreeNode(value)
          
 if __name__ == "__main__":
-    '''tree = BinaryTree(7)
-    tree.root.left = TreeNode(18)
-    tree.root.right = TreeNode(22)
-
-    print(tree.root)
-    print(tree.root.left)
-    print(tree.root.right)'''
-
-    tree = BinaryTree()
-    n1 = TreeNode('a')
-    n2 = TreeNode('+')
-    n3 = TreeNode('*')
-    n4 = TreeNode('b')
-    n5 = TreeNode('-')
-    n6 = TreeNode('/')
-    n7 = TreeNode('c')
-    n8 = TreeNode('d')
-    n9 = TreeNode('e')
-
-    n6.left = n7
-    n6.right = n8
-    n5.left = n6
-    n5.right = n9
-    n3.left = n4
-    n3.right = n5
-    n2.left = n1
-    n2.right = n3
-
-    tree.root = n2
-    tree.simetric_traversal()
-    print()
-
+    pass
