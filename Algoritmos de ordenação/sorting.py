a29a0ebc6d4da0ff90b2c4a357f3c193d0dd8ade
tuliooarauj <127243520+tuliooarauj@users.noreply.github.com>
2023-12-07 16:24:02 -0300

    insertion sort

--- Algoritmos de ordenação/sorting.py
+++ Algoritmos de ordenação/sorting.py
@@ -24,5 +24,19 @@ def bubble_sort(lista):
                 lista[i + 1] = aux
     return lista
 
+def insertion_sort(lista):
+    n = len(lista)
+    for i in range(1, n):
+        chave = lista[i]
+        j = i - 1
+        while j >= 0 and lista[j] > chave:
+            lista[j + 1] = lista[j]
+            j -= 1
+        lista[j + 1] = chave
+    return lista
+
+
+
 print(bubble_sort(lista))
 print(selection_sort(lista))
+print(insertion_sort(lista))
