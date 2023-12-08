5b733915bf1986ee447d4fc53dc68c8a10efc249
tuliooarauj <127243520+tuliooarauj@users.noreply.github.com>
2023-12-08 11:24:29 -0300

    mergesort implementation

--- Algoritmos de ordenação/sorting.py
+++ Algoritmos de ordenação/sorting.py
@@ -35,8 +35,37 @@ def insertion_sort(lista):
         lista[j + 1] = chave
     return lista
 
+def merge_sort(lista, inicio=0, fim=None):
+    if fim is None:
+        fim = len(lista)
+    if fim - inicio > 1: #Caso base
+        meio = (fim + inicio) // 2
+        merge_sort(lista, inicio, meio)
+        merge_sort(lista, meio, fim)
+        merge(lista, inicio, meio, fim)
+    return lista
+
+def merge(lista, inicio, meio, fim):
+    left = lista[inicio:meio]
+    right = lista[meio:fim]
+    top_left = 0
+    top_right = 0
+    for k in range(inicio, fim):
+        if top_left >= len(left):
+            lista[k] = right[top_right]
+            top_right += 1
+        elif top_right >= len(right):
+            lista[k] = left[top_left]
+            top_left += 1
+        elif left[top_left] < right[top_right]:
+            lista[k] = left[top_left]
+            top_left += 1
+        else:
+            lista[k] = right[top_right]
+            top_right += 1
 
 
 print(bubble_sort(lista))
 print(selection_sort(lista))
 print(insertion_sort(lista))
+print(merge_sort(lista))
