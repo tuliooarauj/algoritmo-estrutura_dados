9ce61cdd30c8d711dad2b575c6e458c8ad12cc87
tuliooarauj <127243520+tuliooarauj@users.noreply.github.com>
2023-12-26 12:39:21 -0300

    create linked list

--- Lista 3/q1.py
+++ Lista 3/q1.py
@@ -1,3 +1,61 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+    def __str__(self) -> str: 
+        return str(self.data)
+
+class LinkedList():
+    def __init__(self):
+        self.head = None
+        self._size = 0
+        
+        
+    def inserir(self, element):
+        if self.head: 
+            pointer = self.head
+            while pointer.next: 
+                pointer = pointer.next
+
+            pointer.next = Node(element)
+
+        else:
+            self.head = Node(element)
+
+        self._size +=1 
+    
+    def remove(self, element):
+        if self.head is None:
+            raise ValueError('{} - NOT FOUND'.format(element))
+        elif self.head.data == element:
+            self.head = self.head.next
+            self._size -= 1
+            return 'DELETADO'
+        else:
+            prev = self.head
+            pointer = self.head.next
+            while pointer:
+                if pointer.data == element:
+                    prev.next = pointer.next
+                    pointer.next = None
+                    self._size -= 1
+                    return 'DELETADO'
+                else:
+                    prev = pointer
+                    pointer = pointer.next
+            
+            raise ValueError('{} - NOT FOUND'.format(element))
+
+    def index(self, element): 
+        pointer = self.head
+        i = 0
+
+        while pointer: 
+            if pointer.data == element:
+                return i
+            pointer = pointer.next
+            i += 1
 def return_ascii(character):
     return ord(character)
 
@@ -12,4 +70,20 @@ def hash_function(word, table_size):
     
     return (total_value * 17) % table_size
 
-print(hash_function('aba', 491))
+def main():
+
+    initial_configs = input().split()
+    table_size = int(initial_configs[0])
+    n_entrance = int(initial_configs[1])
+
+    hash_table = [None] * table_size
+
+    i = 0
+    while i < n_entrance:
+        name = input().split()[1]
+        position = hash_function(name, table_size)
+        hash_table[position] = LinkedList()
+        hash_table[position].inserir(name)
+        
+
+main()
